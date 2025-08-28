00b6d1e081fb95dc58ed4c026a37c71fe373e363
Bhaskar Steve <bhaskarsteve76@gmail.com>
2025-08-29 02:51:07 +0530

    eval

--- eval/g31_eval.sh
+++ eval/g31_eval.sh
@@ -1,6 +1,6 @@
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 MODEL="google/gemma-3-1b-it"
-GEN='generation_parameters={"max_new_tokens":2048,"temperature":0.0,"n":1,"best_of":1}'
+GEN='generation_parameters={"max_new_tokens":2048,"temperature":0.0}'
 
 lighteval vllm \
   "model_name=$MODEL,dtype=bfloat16,use_chat_template=True,max_model_length=32768,gpu_memory_utilization=0.90,$GEN" \
--- eval/g34_eval.sh
+++ eval/g34_eval.sh
@@ -1,6 +1,6 @@
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 MODEL="google/gemma-3-4b-it"
-GEN='generation_parameters={"max_new_tokens":2048,"temperature":0.0,"n":1,"best_of":1}'
+GEN='generation_parameters={"max_new_tokens":2048,"temperature":0.0}'
 
 lighteval vllm \
   "model_name=$MODEL,dtype=bfloat16,use_chat_template=True,max_model_length=131072,gpu_memory_utilization=0.90,$GEN" \
