766cab47b9f459c5b3c654448e406f934b528fe6
Bhaskar Steve <bhaskarsteve76@gmail.com>
2025-08-30 17:10:16 +0530

    x

--- eval/g34_eval.sh
+++ eval/g34_eval.sh
@@ -4,5 +4,5 @@ GEN='generation_parameters={"max_new_tokens":2048,"temperature":0.0}'
 
 lighteval vllm \
   "model_name=$MODEL,dtype=bfloat16,use_chat_template=true,max_model_length=131072,gpu_memory_utilization=0.90,$GEN" \
-  "lighteval|math_500|0|0" \
-  --output-dir runs/gemma3-4b-it-math_500 --save-details
+  "lighteval|gpqa|0|0" \
+  --output-dir runs/gemma3-4b-it-gpqa --save-details
