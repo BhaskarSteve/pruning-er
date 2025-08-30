f684f244e8c023a4217e870adc478d76ac100dd9
Bhaskar Steve <bhaskarsteve76@gmail.com>
2025-08-30 17:28:14 +0530

    x

--- data/synthetic.py
+++ data/synthetic.py
@@ -11,9 +11,10 @@ resp = client.chat.completions.create(
 )
 
 msg = resp.choices[0].message
-print("--- reasoning ---")
-print(getattr(msg, "reasoning_content", None))
-print("--- final ---")
-print(getattr(msg, "content", None))
-print("---")
-print("---")
+print(msg)
+# print("--- reasoning ---")
+# print(getattr(msg, "reasoning_content", None))
+# print("--- final ---")
+# print(getattr(msg, "content", None))
+# print("---")
+# print("---")
